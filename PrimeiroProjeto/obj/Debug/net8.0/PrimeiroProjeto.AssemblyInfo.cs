02f074e650df4c4eea6c79211a8dd452e7c92d3a
Marllon Rodrigues <marllonrsantos@gmail.com>
2024-12-20 17:03:11 -0300

    FEAT: Uso de if/else switch/case

--- PrimeiroProjeto/obj/Debug/net8.0/PrimeiroProjeto.AssemblyInfo.cs
+++ PrimeiroProjeto/obj/Debug/net8.0/PrimeiroProjeto.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PrimeiroProjeto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ecbf4db3b194f30130e278afec45e5db6324b3e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64d6cac2803314362e1bcd6586491467701ea047")]
 [assembly: System.Reflection.AssemblyProductAttribute("PrimeiroProjeto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PrimeiroProjeto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
