c69d6c36349394f83f67738856f945bf9f076f8e
Marllon Rodrigues <marllonrsantos@gmail.com>
2024-12-20 18:43:21 -0300

    init: criando o projeto ScreenSound

---
+++ Screensound/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Screensound
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
