8f95daeb805b4c60ed9c2317ae9cc5e4d85b9766
Marllon Rodrigues <marllonrsantos@gmail.com>
2024-12-22 19:07:00 -0300

    FEAT: Criando a classe Album

---
+++ Screensound/Album.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Screensound
+{
+    public class Album
+    {
+        public string Nome { get; set; }
+        public int DuracaoTotal { get; set; }
+
+        public void AdicionarMusica(Musica musica)
+        {
+            
+        }
+    }
+}
--- Screensound/obj/Debug/net8.0/Screensound.AssemblyInfo.cs
+++ Screensound/obj/Debug/net8.0/Screensound.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Screensound")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12b169301081815ec542c698be1734327abd7cc9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d07ceafd8256bb63919dfc53cc0c121a3dcf36a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Screensound")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Screensound")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
