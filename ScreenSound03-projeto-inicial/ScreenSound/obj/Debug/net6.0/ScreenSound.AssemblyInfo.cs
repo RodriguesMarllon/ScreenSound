9580643a123ed55f5bfa574d50a513ae7b98acc9
Marllon Rodrigues <marllonrsantos@gmail.com>
2024-12-22 23:34:31 -0300

    FEAT: Finalizando projeto

--- ScreenSound03-projeto-inicial/ScreenSound/obj/Debug/net6.0/ScreenSound.AssemblyInfo.cs
+++ ScreenSound03-projeto-inicial/ScreenSound/obj/Debug/net6.0/ScreenSound.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ScreenSound")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da5df3df66f8d1f0a45d9d2fecd3b9c80625631b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfaf0d9d18d67b8698eb47912d729c471f8e2287")]
 [assembly: System.Reflection.AssemblyProductAttribute("ScreenSound")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ScreenSound")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
