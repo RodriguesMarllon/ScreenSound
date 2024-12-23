3d454795922c888487c7d9231ef6d92a8d16f866
Marllon Rodrigues <marllonrsantos@gmail.com>
2024-12-22 22:23:55 -0300

    FEAT: Isolando cada opção

--- ScreenSound03-projeto-inicial/ScreenSound/obj/Debug/net6.0/ScreenSound.AssemblyInfo.cs
+++ ScreenSound03-projeto-inicial/ScreenSound/obj/Debug/net6.0/ScreenSound.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ScreenSound")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b3c21a41b0f9b1f411667153bf6aeec3299edb8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b7331dca6bffeba1bf2c1fd23cf31685a46b581")]
 [assembly: System.Reflection.AssemblyProductAttribute("ScreenSound")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ScreenSound")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
