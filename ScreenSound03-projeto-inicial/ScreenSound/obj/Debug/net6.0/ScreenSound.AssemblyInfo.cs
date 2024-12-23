ecc18418f7d32df5f4ff41b30ab010797aa6c17a
Marllon Rodrigues <marllonrsantos@gmail.com>
2024-12-22 21:51:20 -0300

    FEAT: OO no program.cs

--- ScreenSound03-projeto-inicial/ScreenSound/obj/Debug/net6.0/ScreenSound.AssemblyInfo.cs
+++ ScreenSound03-projeto-inicial/ScreenSound/obj/Debug/net6.0/ScreenSound.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ScreenSound")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e91ca98442bb236b7acc3e6bd8282962f45aa92c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc4d9d174c4285e731f8692c3ebf1eab2fdb38e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("ScreenSound")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ScreenSound")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
