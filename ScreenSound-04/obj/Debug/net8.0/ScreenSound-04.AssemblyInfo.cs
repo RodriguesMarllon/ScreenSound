d1a81d7cc3bc78cce794a058f2128aff45108e47
Marllon Rodrigues <marllonrsantos@gmail.com>
2024-12-23 21:58:44 -0300

    FEAT: Realizando uma requisição

--- ScreenSound-04/obj/Debug/net8.0/ScreenSound-04.AssemblyInfo.cs
+++ ScreenSound-04/obj/Debug/net8.0/ScreenSound-04.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ScreenSound-04")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9580643a123ed55f5bfa574d50a513ae7b98acc9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1b93bfd2afb15bf5ebef6b11bf3f4f9e0709081")]
 [assembly: System.Reflection.AssemblyProductAttribute("ScreenSound-04")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ScreenSound-04")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
