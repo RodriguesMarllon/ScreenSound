354f095d4bd3f321d3252e7cbb8a79604be5f15f
Marllon Rodrigues <marllonrsantos@gmail.com>
2024-12-23 23:07:06 -0300

    FEAT: Utilizando Json

--- ScreenSound-04/obj/Debug/net8.0/ScreenSound-04.AssemblyInfo.cs
+++ ScreenSound-04/obj/Debug/net8.0/ScreenSound-04.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ScreenSound-04")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04c659a8ee279fa026f1b8a92c7a300d0235d31e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+567d71ef9a39b0f08191122a05a64efaf483fbdc")]
 [assembly: System.Reflection.AssemblyProductAttribute("ScreenSound-04")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ScreenSound-04")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
