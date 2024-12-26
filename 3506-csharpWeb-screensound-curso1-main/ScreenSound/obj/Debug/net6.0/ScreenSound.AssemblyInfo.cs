4f15b78eca11d945317fe49520a028e6c8e312e2
Marllon Rodrigues <marllonrsantos@gmail.com>
2024-12-25 22:33:19 -0300

    FEAT: Update-Database

--- 3506-csharpWeb-screensound-curso1-main/ScreenSound/obj/Debug/net6.0/ScreenSound.AssemblyInfo.cs
+++ 3506-csharpWeb-screensound-curso1-main/ScreenSound/obj/Debug/net6.0/ScreenSound.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ScreenSound")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3541bb5e014bd23083462974fc207fb35d721903")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d95ad37a9353c2443f831ae3c7cc465b7e98362")]
 [assembly: System.Reflection.AssemblyProductAttribute("ScreenSound")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ScreenSound")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
