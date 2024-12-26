d42827eeb8efaf37f9c777e422682e869cdbb208
Marllon Rodrigues <marllonrsantos@gmail.com>
2024-12-25 22:00:12 -0300

    FEAT: Ajustando o menu

--- 3506-csharpWeb-screensound-curso1-main/ScreenSound/Banco/ArtistaDAL.cs
+++ 3506-csharpWeb-screensound-curso1-main/ScreenSound/Banco/ArtistaDAL.cs
@@ -39,4 +39,9 @@ internal class ArtistaDAL
         context.Artistas.Remove(artista);
         context.SaveChanges();
     }
+
+    public Artista RecuperarPeloNome(string nome)
+    {
+        return context.Artistas.FirstOrDefault(a => a.Nome == nome);
+    }
 }
--- 3506-csharpWeb-screensound-curso1-main/ScreenSound/obj/Debug/net6.0/ScreenSound.AssemblyInfo.cs
+++ 3506-csharpWeb-screensound-curso1-main/ScreenSound/obj/Debug/net6.0/ScreenSound.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ScreenSound")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c159606cfc9fab3ea5e9ac9c4857a23ee13b4b8d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8e7c792974392fb5072268685cd4cc6375b31c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ScreenSound")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ScreenSound")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
