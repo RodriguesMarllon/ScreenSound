7e5a5b3278270087df2b004d8ace2bf8342350a2
Marllon Rodrigues <marllonrsantos@gmail.com>
2024-12-25 22:40:14 -0300

    FEAT: Adicionando uma nova coluna

--- 3506-csharpWeb-screensound-curso1-main/ScreenSound/obj/Debug/net6.0/ScreenSound.AssemblyInfo.cs
+++ 3506-csharpWeb-screensound-curso1-main/ScreenSound/obj/Debug/net6.0/ScreenSound.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ScreenSound")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f15b78eca11d945317fe49520a028e6c8e312e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f0f4208808a3fe86a959df08e341591aebc5b89")]
 [assembly: System.Reflection.AssemblyProductAttribute("ScreenSound")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ScreenSound")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
