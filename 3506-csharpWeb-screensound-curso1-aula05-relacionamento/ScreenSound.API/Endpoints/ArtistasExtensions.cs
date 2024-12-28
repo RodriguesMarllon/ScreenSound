5c882b27b5bae1794b50f1039a3fcdb33df46e4e
Marllon Rodrigues <marllonrsantos@gmail.com>
2024-12-28 03:38:58 -0300

    FEAT: Request

--- 3506-csharpWeb-screensound-curso1-aula05-relacionamento/ScreenSound.API/Endpoints/ArtistasExtensions.cs
+++ 3506-csharpWeb-screensound-curso1-aula05-relacionamento/ScreenSound.API/Endpoints/ArtistasExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ScreenSound.API.Requests;
 using ScreenSound.Banco;
 using ScreenSound.Modelos;
 
@@ -25,8 +26,10 @@ namespace ScreenSound.API.Endpoints
 
             });
 
-            app.MapPost("/Artistas", ([FromServices] DAL<Artista> dal, [FromBody] Artista artista) =>
+            app.MapPost("/Artistas", ([FromServices] DAL<Artista> dal, [FromBody] ArtistaRequest artistaRequest) =>
             {
+                var artista = new Artista(artistaRequest.nome, artistaRequest.bio);
+
                 dal.Adicionar(artista);
                 return Results.Ok();
             });
