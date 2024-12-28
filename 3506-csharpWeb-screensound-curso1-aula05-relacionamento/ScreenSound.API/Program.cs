9451cd28f1ab7829d4e34a5f4bb0bbb4ec8f341c
Marllon Rodrigues <marllonrsantos@gmail.com>
2024-12-28 03:28:33 -0300

    FEAT: Separando as responsabilidades

--- 3506-csharpWeb-screensound-curso1-aula05-relacionamento/ScreenSound.API/Program.cs
+++ 3506-csharpWeb-screensound-curso1-aula05-relacionamento/ScreenSound.API/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using ScreenSound.API.Endpoints;
 using ScreenSound.Banco;
 using ScreenSound.Modelos;
 using System.Data.SqlTypes;
@@ -8,58 +9,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<ScreenSoundContext>();
 builder.Services.AddTransient<DAL<Artista>>();
+builder.Services.AddTransient<DAL<Musica>>();
 
 builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(options => options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
-
 var app = builder.Build();
 
-app.MapGet("/Artistas", ([FromServices] DAL<Artista> dal) => 
-{ 
-    return Results.Ok(dal.Listar());
-}
-);
+app.AddEndPointsArtistas();
+app.AddEndPointMusicas();
 
-app.MapGet("/Artistas/{nome}", ([FromServices] DAL < Artista > dal, string nome) =>
-{
-    var artista =  dal.RecuperarPor(a => a.Nome.ToUpper().Equals(nome.ToUpper()));
-    if (artista is null)
-    {
-        return Results.NotFound();
-    }
-    return Results.Ok(artista);
-}
-);
-
-app.MapPost("/Artistas", ([FromServices] DAL < Artista > dal, [FromBody] Artista artista) =>
-{
-    dal.Adicionar(artista);
-    return Results.Ok();
-});
-
-app.MapDelete("/Artistas/{id}", ([FromServices] DAL<Artista> dal, int id) =>
-{
-    var artista = dal.RecuperarPor(a => a.Id == id);
-    if (artista is null)
-    {
-        return Results.NotFound();
-    }
-    dal.Deletar(artista);
-    return Results.NoContent();
-});
-
-app.MapPut("/Artistas", ([FromServices] DAL<Artista> dal, [FromBody] Artista artista) =>
-{
-    var artistaAAtualizar = dal.RecuperarPor(a => a.Id == artista.Id);
-    if (artistaAAtualizar is null)
-    {
-        return Results.NotFound();
-    }
-    artistaAAtualizar.Nome = artista.Nome;
-    artistaAAtualizar.Bio = artista.Bio;
-    artistaAAtualizar.FotoPerfil = artista.FotoPerfil;
-
-    dal.Atualizar(artistaAAtualizar);
-    return Results.Ok();
-});
 
 app.Run();
